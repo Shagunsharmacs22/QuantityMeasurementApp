8deebc7554ea69ce2ca8732398d7698a25eb92dd
shagunsharmacs22 <shagunmanishsharma@gmail.com>
2026-04-03 00:10:29 +0530

    [Shagun].refactor my front end link on  vercel

--- src/main/java/QuantityMeasurementApplication/config/CorsConfig.java
+++ src/main/java/QuantityMeasurementApplication/config/CorsConfig.java
@@ -1,27 +1,27 @@
 package QuantityMeasurementApplication.config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
-
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://127.0.0.1:5500",
-                    "http://localhost:5500",
-                    "http://localhost:4200",
-                    "http://127.0.0.1:4200",
-                    "https://quantity-measurement-app-frontend-livid.vercel.app"  // ✅ Vercel URL
-                )
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);  // ✅ JWT ke liye zaroori
-            }
-        };
-    }
-}
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.*;
+//
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                .allowedOrigins(
+//                    "http://127.0.0.1:5500",
+//                    "http://localhost:5500",
+//                    "http://localhost:4200",
+//                    "http://127.0.0.1:4200",
+//                    "https://quantity-measurement-app-frontend-livid.vercel.app"  // ✅ Vercel URL
+//                )
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);  // ✅ JWT ke liye zaroori
+//            }
+//        };
+//    }
+//}
